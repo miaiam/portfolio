f03d8158110e075bf61663c06f27c0db90aa40d2
Mia Wölm <Mia@Mias-MacBook-Pro.local>
2017-02-09 18:59:49 +0100

    last changes

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
 		scrollAmount = $(window).scrollTop();
 		if(scrollAmount > 10 && !$('.section-left').hasClass('full-width')){
 			$('.section-left').animate({
-				right: "0%" }, 500, function(){
+				right: "0%" }, 800, function(){
 				});
 			$('.section-left').addClass('full-width');
 			
 		} else if (scrollAmount <= 10 && $('.section-left').hasClass('full-width')){
 			$('.section-left').animate({
-				right: "50%" }, 500, function(){
+				right: "50%" }, 800, function(){
 				});
 			$('.section-left').removeClass('full-width');
 		}
@@ -23,7 +23,7 @@ $(document).ready(function () {
 			}
 
 		if(scrollAmount > $('#intro').outerHeight()){
-			$('#intro').css('top',  ((scrollAmount - $('#intro').outerHeight()) * 0.3) * -1);
+			$('#intro').css('top',  ((scrollAmount - $('#intro').outerHeight()) * 0.2) * -1);
 		}else{
 			$('#intro').css('top', '0');
 		}
